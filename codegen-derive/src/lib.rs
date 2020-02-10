4931ee894d36d5179e2ffa8f83fa7dfa6506d7c5
Yushi OMOTE <yushiomote@gmail.com>
2020-02-11 07:49:24 +0900

    Add per-instruction codegen logic

--- codegen-derive/src/lib.rs
+++ codegen-derive/src/lib.rs
@@ -1,1 +1,147 @@
+extern crate proc_macro;
 
+use nes_codegen::{Addr, Inst};
+use proc_macro::TokenStream as TokenStream1;
+use proc_macro2::{Span, TokenStream};
+use quote::quote;
+use syn::{parse_macro_input, Ident, ItemFn};
+
+fn load() -> Vec<Inst> {
+    serde_yaml::from_str(include_str!("inst.yml")).unwrap()
+}
+
+fn load_by_name<T: ToString>(name: T) -> Vec<Inst> {
+    let name = name.to_string().to_uppercase();
+    load().into_iter().filter(|p| p.name == name).collect()
+}
+
+fn funcname(inst: &Inst) -> Ident {
+    Ident::new(
+        &format!("{}_{:02x}", inst.name.to_lowercase(), inst.opcode),
+        Span::call_site(),
+    )
+}
+
+// See: https://wiki.nesdev.com/w/index.php/CPU_addressing_modes
+fn init(inst: &Inst) -> TokenStream {
+    match inst.addr {
+        Addr::Acc => quote! {
+            let m = &mut reg.a;
+        },
+        Addr::Imp => quote! {},
+        Addr::Imm | Addr::Rel => quote! {
+            let mut _imm = *mmu.get(reg.pc + 1);
+            let m = &mut _imm;
+        },
+        Addr::Zero => quote! {
+            let m = {
+                let imm = *mmu.get(reg.pc + 1);
+                mmu.get_mut(imm as u16)
+            };
+        },
+        Addr::ZeroX => quote! {
+            let m = {
+                let imm = *mmu.get(reg.pc + 1);
+                mmu.get_mut(imm.wrapping_add(reg.x) as u16)
+            };
+        },
+        Addr::ZeroY => quote! {
+            let m = {
+                let imm = *mmu.get(reg.pc + 1);
+                mmu.get_mut(imm.wrapping_add(reg.y) as u16)
+            };
+        },
+        Addr::Abs => quote! {
+            let ma = {
+                mmu.get16(reg.pc + 1)
+            };
+            let m = {
+                mmu.get_mut(ma)
+            };
+        },
+        Addr::AbsX => quote! {
+            let ma = {
+                mmu.get16(reg.pc + 1).wrapping_add(reg.x as u16)
+            };
+            let m = {
+                mmu.get_mut(ma)
+            };
+        },
+        Addr::AbsY => quote! {
+            let ma = {
+                mmu.get16(reg.pc + 1).wrapping_add(reg.y as u16)
+            };
+            let m = {
+                mmu.get_mut(ma)
+            };
+        },
+        Addr::Ind => quote! {
+            let ma = {
+                let p = mmu.get16(reg.pc + 1);
+                mmu.get16(p)
+            };
+            let m = {
+                mmu.get_mut(ma)
+            };
+        },
+        Addr::IndX => quote! {
+            let ma = {
+                let p = *mmu.get(reg.pc + 1);
+                let h = *mmu.get(p.wrapping_add(reg.x) as u16) as u16;
+                let l = *mmu.get(p.wrapping_add(reg.x + 1) as u16) as u16;
+                mmu.get16(h << 8 | l)
+            };
+            let m = {
+                mmu.get_mut(ma)
+            };
+        },
+        Addr::IndY => quote! {
+            let ma = {
+                let p = *mmu.get(reg.pc + 1);
+                let h = *mmu.get(p as u16) as u16;
+                let l = *mmu.get(p.wrapping_add(1) as u16) as u16;
+                let p = h << 8 | l;
+                mmu.get16(p.wrapping_add(reg.y as u16))
+            };
+            let m = {
+                mmu.get_mut(ma)
+            };
+        },
+    }
+}
+
+fn finalize(inst: &Inst) -> TokenStream {
+    let len = inst.len as u16;
+    let cycles = inst.cycles;
+
+    quote! {
+        reg.pc = reg.pc.wrapping_add(#len);
+        #cycles
+    }
+}
+
+#[proc_macro_attribute]
+pub fn op(attr: TokenStream1, item: TokenStream1) -> TokenStream1 {
+    let input = parse_macro_input!(item as ItemFn);
+
+    let mut stream = TokenStream::new();
+
+    let ident = &input.sig.ident;
+    let block = &input.block;
+
+    for inst in load_by_name(&ident) {
+        let pre = init(&inst);
+        let post = finalize(&inst);
+        let ident = funcname(&inst);
+        let result = quote! {
+            fn #ident(reg: &mut Reg, mmu: &mut Mmu) -> usize {
+                #pre
+                #block
+                #post
+            }
+        };
+        stream.extend(result);
+    }
+
+    stream.into()
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,44 +1,121 @@
+use nes_codegen_derive::op;
+use std::cell::RefCell;
+
 pub struct Reg {
+    /// Program counter
     pc: u16,
+    /// Accumulator
     a: u8,
+    /// X index register
     x: u8,
+    /// Y index register
     y: u8,
-    sr: u8,
-    sp: u8,
+    /// Stack pointer
+    s: u8,
+    /// Processor state
+    p: u8,
+}
+
+impl Reg {
+    fn new() -> Self {
+        Self {
+            pc: 0,
+            a: 0,
+            x: 0,
+            y: 0,
+            s: 0xfd,
+            p: 0x34,
+        }
+    }
+}
+
+pub struct Mmu {
+    mem: [u8; 0x1000],
+}
+
+impl Mmu {
+    fn new() -> Self {
+        Self { mem: [0; 0x1000] }
+    }
+
+    fn get(&self, addr: u16) -> &u8 {
+        self.mem.get(addr as usize).unwrap()
+    }
+
+    fn get_mut(&mut self, addr: u16) -> &mut u8 {
+        self.mem.get_mut(addr as usize).unwrap()
+    }
+
+    fn get16(&self, addr: u16) -> u16 {
+        let h = *self.get(addr) as u16;
+        let l = *self.get(addr.wrapping_add(1)) as u16;
+        h << 8 | l
+    }
 }
 
 macro_rules! c {
     ($e:ident) => {
-        $e.sr & 1
+        $e.p & 1
     };
     ($e:ident, $v:expr) => {
-        $e.sr |= ($v as u8)
+        $e.p = ($e.p & !0x01) | ($v as u8)
     };
 }
 
 macro_rules! n {
     ($e:ident, $v:expr) => {
-        $e.sr = ($e.sr & !0x80) | ($v & 0x80)
+        $e.p = ($e.p & !0x80) | ($v & 0x80)
     };
 }
 
 macro_rules! z {
     ($e:ident, $v:expr) => {
         if $v == 0 {
-            $e.sr &= !0x40;
+            $e.p |= 0x02;
         } else {
-            $e.sr |= 0x40;
+            $e.p &= !0x02;
         }
     };
 }
 
-// #[opcode]
+#[test]
+fn flg_c() {
+    let mut r = Reg::new();
+    r.p = 0xf0;
+    c!(r, true);
+    assert_eq!(r.p, 0xf1);
+    c!(r, false);
+    assert_eq!(r.p, 0xf0);
+}
+
+#[test]
+fn flg_z() {
+    let mut r = Reg::new();
+    r.p = 0xf0;
+    z!(r, 0);
+    assert_eq!(r.p, 0xf2);
+    z!(r, 10);
+    assert_eq!(r.p, 0xf0);
+}
+
+#[test]
+fn flg_n() {
+    let mut r = Reg::new();
+    r.p = 0x0f;
+    n!(r, 0x80);
+    assert_eq!(r.p, 0x8f);
+    n!(r, 0x30);
+    assert_eq!(r.p, 0x0f);
+}
+
+#[op]
 fn adc(reg: &mut Reg, m: &mut u8) {
     let (v, o1) = reg.a.overflowing_add(*m);
     let (v, o2) = reg.a.overflowing_add(c!(reg));
     reg.a = v;
     c!(reg, o1 || o2);
     n!(reg, reg.a);
+    z!(reg, reg.a);
 }
 
 pub fn run() {}
